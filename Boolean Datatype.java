28ee6d33ea07caa07c410d55b4b0790e6b6565c9
Cherish Sharma <cherish.sharma2007@gmail.com>
2026-09-24 18:30:46 +0530

    Create Boolean Datatype.java

---
+++ Boolean Datatype.java
@@ -0,0 +1,9 @@
+public class Main{
+  public static void main(string[]args)
+  {
+    boolean isJavaFun = true;
+    boolean isFishTasty = false;
+    system.out.println("Is Java fun ?" +isJavaFun);
+    system.out.println("Is fish tasty ?" +isFishTasty);
+  }
+}
