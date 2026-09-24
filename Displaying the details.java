23979416f24312bd1aa7bc9db6e1926daac3003a
Cherish Sharma <cherish.sharma2007@gmail.com>
2026-09-24 12:43:30 +0530

    Create Displaying the details.java

---
+++ Displaying the details.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main (string[])args){
+        int age = 25;
+        string name = "Rahul";
+        double salary = 50000.00;
+        system.out.println("Age:", +age);
+        system.out.println("Name:", +name);
+        system.out.println("Salary:", +salary);
+    }
+}
