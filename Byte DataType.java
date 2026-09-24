08eac4e772b5c281d619f3f411b9a8f874e1ce70
Cherish Sharma <cherish.sharma2007@gmail.com>
2026-09-24 18:33:08 +0530

    Create Byte DataType class with main method
    Fix class and method names, and correct output statements.

---
+++ Byte DataType.java
@@ -0,0 +1,9 @@
+public class main {
+  public static void main(string[] args)
+  {
+    byte age = 25;
+    byte temperature = -10;
+    system.out.println("Age: " +age);
+    system.out.println("Temperature:" +temperature);
+  }
+}
