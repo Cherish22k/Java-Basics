17fe62696916a5669954c8a7230acb7fdc9212ab
Cherish Sharma <cherish.sharma2007@gmail.com>
2026-09-24 13:01:18 +0530

    Create Initializing Code float , int , char.java

---
+++ Initializing Code float , int , char.java
@@ -0,0 +1,12 @@
+class Main{
+  public static void main(string[] args){
+    float si = 10;
+    int t = 10;
+    int s =20;
+    char var = 'h';
+   system.out.println("Simple Interest:" , +si);
+   system.out.println("Speed:" , +s);
+   system.out.println("Time:" , +t);
+   system.out.println("Character:" , +var);
+    }
+}
